9a15bd449185bbbae03cb0eec12eced89ba0b7c1
Thomas <bocquet.t@gmail.com>
2018-05-14 23:19:04 +0200

    Compute Real Hash / ToString Method for Block / Testing

--- kudoChain.go
+++ kudoChain.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"strconv"
+	"time"
 )
 
 //struct Block
@@ -16,13 +20,24 @@ type Block struct {
 	timestamp     string
 	previousOwner string
 	newOwner      string
+	hash          string
 }
 
-func computeHash(block Block) string {
-	hashString := strconv.Itoa(block.id) + block.previousHash + block.timestamp + block.previousOwner + block.newOwner
-	return hashString
+func (block Block) ToString() string {
+	return strconv.Itoa(block.id) + block.previousHash + block.timestamp + block.previousOwner + block.newOwner
+}
+
+func (block *Block) computeHash() {
+	hashString := block.ToString()
+	hash := sha256.New()
+	hash.Write([]byte(hashString))
+	sha := hash.Sum(nil)
+	block.hash = hex.EncodeToString(sha)
 }
 
 func main() {
+	genesis := Block{1, "", time.Now().String(), "", "Thomas", ""}
+	genesis.computeHash()
+	fmt.Printf(genesis.hash)
 
 }
---
+++ kudochain_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+type hashTestPair struct {
+	block        Block
+	expectedHash string
+}
+
+var tests = []hashTestPair{
+	{Block{1, "", "timestamp", "", "Thomas", ""}, "df9f931e333586ff29d2e7e1700db2728f06158d0863e6445ca610c8d5f96e30"}}
+
+func TestComputeHash(t *testing.T) {
+	for _, pair := range tests {
+		pair.block.computeHash()
+		if pair.block.hash != pair.expectedHash {
+			t.Error("For : ", pair.block.ToString(),
+				"\n , expected :", pair.expectedHash,
+				"\n, but was :", pair.block.hash)
+		}
+	}
+}
+
+func TestHashUnicity(t *testing.T) {
+	block1 := Block{1, "", "timestamp", "", "Thomas", ""}
+	block2 := Block{1, "", "timestamp", "", "Thomas", ""}
+	block1.computeHash()
+	block2.computeHash()
+	if block1.hash != block2.hash {
+		t.Error("Block 1 : ", block1.ToString(), "Block 2 : ", block2.ToString(), "Got Different Hashes")
+	}
+}
